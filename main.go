7d799a89653bcd53d730edc9e42b30dd85556292
Kristian Mide <fas@mide.dk>
2018-10-18 21:47:09 +0200

    UI rewrite from termui to gocui

--- main.go
+++ main.go
@@ -5,16 +5,22 @@ import (
 	"fmt"
 	"log"
 	"sort"
-	"sync"
 	"time"
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/fasmide/capture-all-the-scripts/server"
-	ui "github.com/gizak/termui"
+	"github.com/fatih/color"
+	"github.com/jroimartin/gocui"
 )
 
 var (
 	port = flag.Int("port", 22, "specify listen port")
+)
+
+var (
+	activeConnView *gocui.View
+	statsView      *gocui.View
+	logView        *gocui.View
 )
 
 func main() {
@@ -33,112 +39,114 @@ func main() {
 }
 
 func gui(server *server.SSH, events chan string) {
-	err := ui.Init()
+
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		panic(err)
+		log.Panicln(err)
 	}
-	defer ui.Close()
+	defer g.Close()
 
-	strs := []string{
-		"[0] github.com/gizak/termui",
-		"[1] [你好，世界](fg-blue)",
-		"[2] [こんにちは世界](fg-red)",
-		"[3] [color output](fg-white,bg-green)",
-		"[4] output.go",
-		"[5] random_out.go",
-		"[6] dashboard.go",
-		"[7] nsf/termbox-go"}
+	g.SetManagerFunc(layout)
 
-	ls := ui.NewList()
-	ls.Items = strs
-	ls.ItemFgColor = ui.ColorYellow
-	ls.BorderLabel = "(%d) Active Connections"
-	ls.Height = 7
-	ls.Y = 0
-
-	par := ui.NewPar("Total conns: \t%d\nTotal bytes: \t%d")
-	par.Height = 4
-	par.BorderLabel = "Stats"
-
-	log := ui.NewList()
-	log.ItemFgColor = ui.ColorYellow
-	log.BorderLabel = "Log"
-	log.Height = 7
-
-	var renderLock sync.Mutex
-
-	// build layout
-	ui.Body.AddRows(
-		ui.NewRow(
-			ui.NewCol(6, 0, ls),
-			ui.NewCol(6, 0, par),
-		),
-		ui.NewRow(
-			ui.NewCol(12, 0, log),
-		),
-	)
-
-	// calculate layout
-	ui.Body.Align()
-
-	ui.Render(ui.Body)
-
-	ui.Handle("q", func(ui.Event) {
-		ui.StopLoop()
-	})
-
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	started := time.Now()
 	go func() {
 		for {
-			renderLock.Lock()
 			s := server.State()
-			sort.Sort(SortByStarted(s.Connections))
-			list := make([]string, len(s.Connections))
-			activeBytes := 0
-			for i, item := range s.Connections {
-				list[i] = fmt.Sprintf("%11s: %7s: %s", time.Now().Sub(item.Started).Truncate(time.Second), humanize.Bytes(uint64(item.BytesSent)), item.Remote)
-				activeBytes += item.BytesSent
-			}
-			ls.Items = list
-			ls.BorderLabel = fmt.Sprintf("(%d) Active connections", len(list))
+			sort.Sort(byStarted(s.Connections))
 
-			par.Text = fmt.Sprintf("Total conns: \t%d\nTotal bytes: \t%s", s.TotalConnections, humanize.Bytes(uint64(s.BytesSent+activeBytes)))
-			ui.Render(ui.Body)
-			renderLock.Unlock()
+			g.Update(func(g *gocui.Gui) error {
+
+				activeConnView.Clear()
+				activeBytes := 0
+				for _, item := range s.Connections {
+					color.New(color.FgGreen).Fprintf(activeConnView, "%11s: %7s: %s\n",
+						time.Now().Sub(item.Started).Truncate(time.Second).String(),
+						humanize.Bytes(uint64(item.Written())),
+						item.Remote,
+					)
+					activeBytes += item.Written()
+				}
+				activeConnView.Title = fmt.Sprintf("(%d) Active connections", len(s.Connections))
+				statsView.Clear()
+				fmt.Fprintf(statsView, "Total conns: \t%d\nTotal bytes: \t%s\nUptime: \t\t%7s\n",
+					s.TotalConnections,
+					humanize.Bytes(uint64(s.BytesSent+activeBytes)),
+					time.Now().Sub(started).Truncate(time.Second),
+				)
+
+				return nil
+			})
 
 			time.Sleep(time.Millisecond * 500)
 		}
 	}()
 
 	go func() {
+		eventSlice := make([]string, 0, 50)
 		for {
 			event := <-events
-			renderLock.Lock()
-			log.Items = append([]string{event}, log.Items...)
-			// cap the log to 100 entries
-			if len(log.Items) >= 100 {
-				log.Items = log.Items[:100]
+			eventSlice = append(eventSlice, event)
+			if len(eventSlice) >= 30 {
+				eventSlice = eventSlice[len(eventSlice)-29 : 30]
 			}
-			renderLock.Unlock()
+			g.Update(func(g *gocui.Gui) error {
+				logView.Clear()
+				for _, s := range eventSlice {
+					color.New(color.FgYellow).Fprintf(logView, "%s\n", s)
+				}
+				return nil
+			})
+
 		}
 	}()
 
-	ui.Handle("<Resize>", func(e ui.Event) {
-		renderLock.Lock()
-		defer renderLock.Unlock()
-		payload := e.Payload.(ui.Resize)
-		ui.Body.Width = payload.Width
-		height := payload.Height / 2
-		ls.Height = height
-		log.Height = height
-		ui.Body.Align()
-		ui.Clear()
-		ui.Render(ui.Body)
-	})
-	ui.Loop()
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
 }
 
-type SortByStarted []*server.Connection
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
 
-func (s SortByStarted) Len() int           { return len(s) }
-func (s SortByStarted) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-func (s SortByStarted) Less(i, j int) bool { return s[i].Started.Before(s[j].Started) }
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	var err error
+	if activeConnView, err = g.SetView("activeconnections", 0, 0, maxX/2-1, maxY/2-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		activeConnView.Wrap = true
+		activeConnView.Title = "(%d) Active Connections"
+	}
+
+	if statsView, err = g.SetView("stats", maxX/2-1, 0, maxX-1, maxY/2-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		statsView.Title = "Stats"
+	}
+
+	if logView, err = g.SetView("log", 0, maxY/2-1, maxX-1, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		logView.Title = "Log"
+		logView.Autoscroll = true
+	}
+
+	return nil
+}
+
+type byStarted []*server.Connection
+
+func (s byStarted) Len() int           { return len(s) }
+func (s byStarted) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s byStarted) Less(i, j int) bool { return s[i].Started.Before(s[j].Started) }
